7ebc39739670eb70e969f13552ec1691ff8574b4
Daniel Lin <Daniel_Lin@outlook.com>
2017-04-10 13:48:29 +0200

    rip

--- DACTest/core/src/Game/Map/TiledMapClickListener.java
+++ DACTest/core/src/Game/Map/TiledMapClickListener.java
@@ -31,22 +31,18 @@ public class TiledMapClickListener extends ClickListener {
 
     @Override
     public void clicked(InputEvent event, float x, float y) {
-        System.out.println("X:" + actor.getX() + " Y:" + actor.getY() + " has been clicked.");
+
 
         //ToDo: om units te spawnen om te testen pas dit aan
         //stage.createUnit(actor);
-        if(Gdx.input.isButtonPressed(Input.Buttons.LEFT)){
+            System.out.println("X:" + actor.getX() + " Y:" + actor.getY() + " has been clicked.");
             for (int i = 0; i < units.size() && units.size() != 0; i++) {
                 if (actor.getX() == units.get(i).getCoordinate().getX() && actor.getY() == units.get(i).getCoordinate().getY()) {
                     units.get(i).setSelected(true);
                 } else {
                     units.get(i).setSelected(false);
                 }
-
-
         //stage.createUnit(actor); // gebruik dit om te debuggen en units onClick te spawnen.
-
-
             }
 
             for (int i = 0; i < buildings.size() && buildings.size() != 0; i++)
@@ -61,8 +57,6 @@ public class TiledMapClickListener extends ClickListener {
                 }
             }
         }
-
-    }
 
     @Override
     public void enter(InputEvent event, float x, float y, int pointer, Actor fromActor) {
